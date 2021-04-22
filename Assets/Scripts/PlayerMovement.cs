b16aaa4703f8e430b7023e0d2cbd43d93d47a338
OhNoPano <78089715+OhNoPano@users.noreply.github.com>
2021-04-22 11:35:55 -0500

    Movement (no Button though)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    public CharacterController2D controller;
-    public float runSpeed = 40f;
-    float horizontalMove = 0f;
+
+    public float moveSpeed = 10f;
+    public float bigJump = 10f;
+    public bool isGrounded = false;
+
+    private bool Left = false;
+    private bool Right = false;
+    private bool buttonJump = false;
+
     // Update is called once per frame
     void Update () {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        Jump();
+        Vector3 horizontalMove = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += horizontalMove * Time.deltaTime * moveSpeed;
     }
 
-    void FixedUpdate () {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    void Jump () {
+        if (Input.GetButtonDown("Jump") && isGrounded == true){
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, bigJump), ForceMode2D.Impulse);
+
+        }
     }
 }
