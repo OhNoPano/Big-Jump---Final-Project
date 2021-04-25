ad9f2fcb8d4caa73e4491af425cd957c1db0446a
OhNoPano <78089715+OhNoPano@users.noreply.github.com>
2021-04-25 16:46:46 -0500

    Movement really works :)
    Sometimes the ground isn't being read idk

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,19 +10,41 @@ public class PlayerMovement : MonoBehaviour {
 
     private bool Left = false;
     private bool Right = false;
-    private bool buttonJump = false;
 
     // Update is called once per frame
     void Update () {
         Jump();
         Vector3 horizontalMove = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += horizontalMove * Time.deltaTime * moveSpeed;
+        if(Left == true){
+            transform.position += transform.right * Time.deltaTime * -moveSpeed;
+        }
+        if(Right == true){
+            transform.position += transform.right * Time.deltaTime * moveSpeed;
+        }
     }
 
     void Jump () {
         if (Input.GetButtonDown("Jump") && isGrounded == true){
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, bigJump), ForceMode2D.Impulse);
 
+        }
+    }
+    public void leftClick(){
+        Left = true;
+    }
+    public void leftClickUp(){
+        Left = false;
+    }
+    public void rightClick(){
+        Right = true;
+    }
+    public void rightClickUp(){
+        Right = false;
+    }
+    public void jumpClick(){
+        if (isGrounded == true){
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, bigJump), ForceMode2D.Impulse);
         }
     }
 }
