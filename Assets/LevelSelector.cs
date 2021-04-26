dfb1efe3129aa3ed9cf84d4b9210577f6aa2ffe5
Julian Trujillo <juliantrujillo037@gmail.com>
2021-04-26 12:01:53 -0500

    level selector

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public void PlayLevel1 () {
+        SceneManager.LoadScene(2);
+    }
+    //public void PlayLevel2 () {
+    //  SceneManager.LoadScene(3);
+    //}
+    //public void PlayLevel3 () {
+    // SceneManager.LoadScene(4);
+    //}
+}
