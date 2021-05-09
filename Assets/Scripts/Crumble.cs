2ba9033cd31ab636cd4397d30e8dbbcf5b834e03
tommysalamivevo <78112780+tommysalamivevo@users.noreply.github.com>
2021-05-09 14:15:21 -0500

    problem?

--- Assets/Scripts/Crumble.cs
+++ Assets/Scripts/Crumble.cs
@@ -6,18 +6,28 @@ public class Crumble : MonoBehaviour
 {
 
     public float fallDelay = 3.0f;
+    
+    private Vector3 location;
+
+    private bool collided = false;
+
+    public Rigidbody2D rb;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        location = transform.position;
+        collided = false;
     }
     void OnCollisionEnter2D (Collision2D col)
     {
-        if (col.gameObject.name == "Player")
+        if (col.gameObject.name == "Player" && collided == false)
         {
+            collided = true;
             StartCoroutine(fallAfterDelay());
+            collided = false;
         }
         
     }
@@ -26,6 +36,14 @@ public class Crumble : MonoBehaviour
     {
         yield return new WaitForSeconds(fallDelay);
         GetComponent<Rigidbody2D>().isKinematic = false;
+        yield return new WaitForSeconds(3f);
+        transform.position = location;
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        rb.velocity = Vector3.zero;
+        rb.Sleep();
+        //rb.angularVelocity = Vector3.zero;
     }
+
+    
 
 }
