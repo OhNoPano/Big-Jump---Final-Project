096ed02c92ca9699be88f7d2673ef3b9306cf744
OhNoPano <78089715+OhNoPano@users.noreply.github.com>
2021-05-10 11:42:31 -0500

    Nothing

--- Assets/Scripts/Crumble.cs
+++ Assets/Scripts/Crumble.cs
@@ -6,28 +6,18 @@ public class Crumble : MonoBehaviour
 {
 
     public float fallDelay = 3.0f;
-    
-    private Vector3 location;
-
-    private bool collided = false;
-
-    public Rigidbody2D rb;
-
 
 
     // Start is called before the first frame update
     void Start()
     {
-        location = transform.position;
-        collided = false;
+    
     }
     void OnCollisionEnter2D (Collision2D col)
     {
-        if (col.gameObject.name == "Player" && collided == false)
+        if (col.gameObject.name == "Player")
         {
-            collided = true;
             StartCoroutine(fallAfterDelay());
-            collided = false;
         }
         
     }
@@ -36,14 +26,6 @@ public class Crumble : MonoBehaviour
     {
         yield return new WaitForSeconds(fallDelay);
         GetComponent<Rigidbody2D>().isKinematic = false;
-        yield return new WaitForSeconds(3f);
-        transform.position = location;
-        GetComponent<Rigidbody2D>().isKinematic = true;
-        rb.velocity = Vector3.zero;
-        rb.Sleep();
-        //rb.angularVelocity = Vector3.zero;
     }
-
-    
 
 }
