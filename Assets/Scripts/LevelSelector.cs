cd3616c3b42a1f942c959a7cd408a20a35718477
Julian Trujillo <juliantrujillo037@gmail.com>
2021-05-11 23:28:55 -0500

    consolidated mainmenu + levelselector
    HI JAY I LOVE U

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -6,12 +6,12 @@ using UnityEngine.SceneManagement;
 public class LevelSelector : MonoBehaviour
 {
     public void PlayLevel1 () {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
     public void PlayLevel2 () {
-      SceneManager.LoadScene(3);
+      SceneManager.LoadScene(2);
     }
     //public void PlayLevel3 () {
-    // SceneManager.LoadScene(4);
+    // SceneManager.LoadScene(3);
     //}
 }
