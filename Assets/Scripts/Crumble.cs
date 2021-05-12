7d513ee3840d83a477040b058f81b3ceae57d163
tommysalamivevo <78112780+tommysalamivevo@users.noreply.github.com>
2021-05-12 12:44:12 -0500

    level 2 is done and the flags work too
    🏁 which pride flag is this

--- Assets/Scripts/Crumble.cs
+++ Assets/Scripts/Crumble.cs
@@ -7,17 +7,29 @@ public class Crumble : MonoBehaviour
 
     public float fallDelay = 3.0f;
 
+    public float deleteDelay = 3.0f;
+    
+    private Vector3 location;
+
+    private bool collided = false;
+    
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        location = transform.position;
+        collided = false;
     }
     void OnCollisionEnter2D (Collision2D col)
     {
-        if (col.gameObject.name == "Player")
+        if (col.gameObject.name == "Player" && collided == false)
         {
+            collided = true;
             StartCoroutine(fallAfterDelay());
+            collided = false;
         }
         
     }
@@ -26,6 +38,11 @@ public class Crumble : MonoBehaviour
     {
         yield return new WaitForSeconds(fallDelay);
         GetComponent<Rigidbody2D>().isKinematic = false;
+        yield return new WaitForSeconds(deleteDelay);
+        transform.position = location;
+        GetComponent<Rigidbody2D>().isKinematic = true;
     }
+
+    
 
 }
